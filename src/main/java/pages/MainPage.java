6ebcc75f3e80b3b3fc0e4bff5a221d871fdc0f77
Ann Belova <tmpmouse@gmail.com>
2024-03-31 18:28:34 +0300

    add struct

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,8 +1,19 @@
 package pages;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import jakarta.inject.Inject;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class MainPage extends AbsBasePage<MainPage>{
-    private String selector
+    private SelenideElement dwaitDownloadMainPage = $("[text = 'Chat to improve your English']");
 
 }
+//android.widget.TextView[@text="Chat to improve your English"]
+//android.widget.TextView[@text="Next"]
+//android.widget.TextView[@text="Learn new words and grammar"]
+//android.widget.TextView[@text="7 days FREE"]
+//android.widget.TextView[@text="Skip >"]
+//android.widget.TextView[@resource-id="android:id/message"]
+//android.widget.Button[@resource-id="android:id/button1"]
